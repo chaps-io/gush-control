5cb108b97df9b86f9d5ee82181020857061cac44
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2014-05-27 14:45:00 +0200

    register job to start workflow

--- lib/gush/control/app.rb
+++ lib/gush/control/app.rb
@@ -4,6 +4,15 @@ module Gush
       get "/" do
         slim :index
       end
+
+      post "/run/:workflow" do |workflow|
+        cli = Gush::CLI.new
+
+        id = cli.create(workflow)
+        cli.start(id)
+
+        id
+      end
     end
   end
 end
