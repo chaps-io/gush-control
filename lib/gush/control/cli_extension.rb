57c18352dbfbc58205d4e33eeaa8f8b5e535d811
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2014-05-29 12:54:01 +0200

    updated thin settings

--- lib/gush/control/cli_extension.rb
+++ lib/gush/control/cli_extension.rb
@@ -1,10 +1,23 @@
 module Gush
   class CLI < Thor
     desc "server [options]", "Start server"
-    option :port
+    option :port, aliases: "-p", default: "3000"
+    option :address, aliases: "-a", default: "0.0.0.0"
+    option :daemonize, aliases: "-d"
+
     def server
-      puts "Starting server!"
-      Thin::Runner.new(["start", "-R", Gush::Control.rackup_path.to_s]).run!
+      Thin::Runner.new(params_to_args(thin_params(options))).run!
+    end
+
+    private
+    def thin_params(options)
+      { rackup: Gush::Control.rackup_path.to_s, port: options[:port], address: options[:address] }.tap do |params|
+        params.merge!(daemonize: true) if options[:daemonize]
+      end
+    end
+
+    def params_to_args(params)
+      params.flat_map{|k, v| ["--#{k}", v] }.unshift("start")
     end
   end
 end
